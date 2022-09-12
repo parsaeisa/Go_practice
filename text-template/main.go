eacc0f92b37cc83c73de316651483186fdc13413
parsaeissazadeh <parsa.eissazadeh@snapp.cab>
2022-09-12 16:38:09 +0430

    create main file and bring one example of template

---
+++ text-template/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	print("hello")
+}
+
+//contentTemplate := template.New("content-template")
+//contentTemplate, err = contentTemplate.Parse("کاربر راننده عزیز، خسارت لغو سفر، برای سفر کد {{.HRI}}، در تاریخ: {{.DateTime}}، به اعتبار شما اضافه شد.")
+//if err != nil {
+//return err
+//}
+//
+//var tpl bytes.Buffer
+//err = contentTemplate.Execute(&tpl, map[string]interface{}{"HRI": c.rideEvent.HRI, "DateTime": now})
+//if err != nil {
+//return err
+//}
