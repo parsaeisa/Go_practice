137a804520f7a068ee0caa1f3f57913008e71557
parsaeissazadeh <parsa.eissazadeh@snapp.cab>
2022-09-13 11:07:17 +0430

    playing around with text/template

--- text-template/main.go
+++ text-template/main.go
@@ -1,17 +1,33 @@
 package main
 
-func main() {
-	print("hello")
+import (
+	"bytes"
+	"text/template"
+)
+
+type Car struct {
+	// for template , attributes must be exposable
+	Lat float32
+	Lng float32
 }
 
-//contentTemplate := template.New("content-template")
-//contentTemplate, err = contentTemplate.Parse("کاربر راننده عزیز، خسارت لغو سفر، برای سفر کد {{.HRI}}، در تاریخ: {{.DateTime}}، به اعتبار شما اضافه شد.")
-//if err != nil {
-//return err
-//}
-//
-//var tpl bytes.Buffer
-//err = contentTemplate.Execute(&tpl, map[string]interface{}{"HRI": c.rideEvent.HRI, "DateTime": now})
-//if err != nil {
-//return err
-//}
+func main() {
+	car := Car{
+		Lat: 12.2,
+		Lng: 13.2,
+	}
+	c, err := template.New("content-template").Parse("خودروی شما در موقعیت {{.Lat}} و {{.Lng}} قرار دارد .")
+	if err != nil {
+		panic(err)
+	}
+
+	// what is bytes buffer ?
+	var tpl bytes.Buffer
+	err = c.Execute(&tpl, car)
+	if err != nil {
+		return
+	}
+
+	content := tpl.String()
+	println(content)
+}
