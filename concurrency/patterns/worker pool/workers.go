35fd932218d570737679be065acd74f4844fbc40
parsaeissazadeh <parsa.eissazadeh@snapp.cab>
2022-10-12 13:50:23 +0330

    define jobs

--- concurrency/patterns/worker pool/workers.go
+++ concurrency/patterns/worker pool/workers.go
@@ -1,5 +1,31 @@
 package worker_pool
 
+import "context"
+
 type Worker struct {
 	In, Out chan string
+}
+
+func (t Worker) Print(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case payload := <-t.In:
+			println(payload)
+			t.Out <- payload
+		}
+	}
+}
+
+func (t Worker) Check(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case payload := <-t.In:
+			println(payload, " checked")
+			t.Out <- payload + ":checked"
+		}
+	}
 }
